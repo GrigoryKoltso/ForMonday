b4f248a9778f311300611bbabdd722f5c725a335
GrigoryKoltso <154324680+GrigoryKoltso@users.noreply.github.com>
2024-02-03 15:04:00 +0100

    Add files via upload

---
+++ HomeworkForMonday/main.js
@@ -0,0 +1,47 @@
+
+   /*      ДЗ.
+    1. Найти сумму четных и нечетных чисел из массива и вывести в консоли разницу между ними
+    2. Найти сумму четных и нечетных чисел из массива и вывести их сумму в консоли
+    3. Найти сумму отрицательных чисел из массива
+*/
+
+   /* const numbers = [11, 20, 53, 1, 18, 40, 57, 70, 109]
+   let chet = 0
+   for(i =0; i < numbers.length; i++) {
+    if(numbers[i] % 2 === 0){
+      chet = numbers[i] + chet
+    }
+   }
+   console.log(chet)
+   let nechet = 0
+   for(i =0; i < numbers.length; i++) {
+    if(numbers[i] % 2 !== 0){
+      nechet = numbers[i] + nechet
+    }
+   }
+   console.log(nechet)
+   console.log(nechet - chet) */
+
+   
+   /* const numbers = [11, 20, 53, 1, 18, 40, 57, 70, 109]
+    let chet = 0
+    let nechet = 0
+   for(i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 2 === 0) {
+      chet = chet + numbers[i]
+    } else if(numbers[i] != 0) {
+      nechet = nechet + numbers[i]
+    }
+   }
+   console.log(chet);
+   console.log(nechet);
+   console.log(chet +++ nechet); */
+
+   /* const numbers = [-10, 20, -50, 1, -15, -40, -58, 70, 100]
+   sum = 0
+   for(i = 0; i < numbers.length; i++) {
+    if(numbers[i] < 0) {
+      sum = sum + numbers[i]
+    }
+   }
+   console.log(sum) */
